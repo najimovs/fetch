2b9c6a0e4f92819b491853077c858f77198fabc8
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-07-18 16:49:23 +0500

    Add todo list UI and styles
    Introduces a basic todo list rendered in the DOM with sample data. Adds a new CSS file for layout and completed item styling, and updates index.html to include the todo list container.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,54 @@
-console.log( 0 )
+import "./main.css"
+
+const todos = [
+	{
+		title: "Todo #1",
+		completed: true,
+	},
+	{
+		title: "Todo #2",
+		completed: false,
+	},
+	{
+		title: "Todo #3",
+		completed: true,
+	},
+]
+
+const ulElement = document.querySelector( "ul" )
+
+for ( const todo of todos ) {
+
+	const liElement = document.createElement( "li" )
+	const titleElement = document.createElement( "span" )
+
+	titleElement.className = "title"
+	// liElement.className = todo.completed ? "completed" : ""
+
+	if ( todo.completed ) {
+
+		liElement.classList.add( "completed" )
+	}
+
+	liElement.appendChild( titleElement )
+
+	titleElement.textContent = todo.title
+
+	ulElement.appendChild( liElement )
+}
+
+// TERNARY OPERATOR
+
+// const age = 14
+
+// console.log( age > 14 ? "OK" : 20 > 10 ? "A" : "B" )
+// console.log( age > 14 ? "OK" : 20 > 10 ? "A" : "B" )
+
+// if ( age > 14 ) {
+
+// 	console.log( "OK" )
+// }
+// else {
+
+// 	console.log( "NOT" )
+// }
